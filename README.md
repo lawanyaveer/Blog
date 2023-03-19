e044d1c0c04e92b21f3abf42045b7fb8437ea3f5
lawanyaveer <lawanyaveer@gmail.com>
2023-03-19 13:09:55 +0530

    phase 1 code

---
+++ README.md
@@ -0,0 +1,3 @@
+# Blog-Project
+
+This is a Blog site . It uses Nodejs and it's frame works and semantic ui for styling.
---
+++ app.js
@@ -0,0 +1,97 @@
+var express = require("express"),
+	mongoose = require("mongoose"),
+	bodyParser = require("body-parser"),
+	app = express();
+
+mongoose.connect("mongodb://localhost/restful_blog_App",{ useNewUrlParser: true });
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+var blogSchema = new mongoose.Schema({
+	title: String,
+	image: String,
+	body: String,
+	created: {type:Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog",blogSchema);
+
+app.get("/",function(req,res){
+	res.redirect("/blogs");
+});
+app.get("/blogs",function(req,res){
+	Blog.find({},function(err, blogs){
+		if(err){
+			console.log(err);
+		}else{
+			res.render("index",{blogs: blogs});
+		}
+	})
+	
+});
+
+app.get("/blogs/new", function(req, res){
+   res.render("new"); 
+});
+
+app.post("/blogs", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+   var formData = req.body.blog;
+   Blog.create(formData, function(err, newBlog){
+       console.log(newBlog);
+      if(err){
+          res.render("new");
+      } else {
+          res.redirect("/blogs");
+      }
+   });
+});
+
+app.get("/blogs/:id", function(req, res){
+   Blog.findById(req.params.id, function(err, blog){
+      if(err){
+          res.redirect("/");
+      } else {
+          res.render("show", {blog: blog});
+      }
+   });
+});
+
+app.get("/blogs/:id/edit", function(req, res){
+   Blog.findById(req.params.id, function(err, blog){
+       if(err){
+           console.log(err);
+           res.redirect("/")
+       } else {
+           res.render("edit", {blog: blog});
+       }
+   });
+});
+
+app.put("/blogs/:id", function(req, res){
+   Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, blog){
+       if(err){
+           console.log(err);
+       } else {
+         var showUrl = "/blogs/" + blog._id;
+         res.redirect(showUrl);
+       }
+   });
+});
+
+app.delete("/blogs/:id", function(req, res){
+   Blog.findByIdAndRemove(req.params.id, function(err){
+       if(err){
+           res.redirect("/blogs");
+       }else{
+           res.redirect("/blogs");
+       }
+   }); 
+});
+
+
+
+app.listen(process.env.PORT,process.env.IP,function(){
+	console.log("server is running");
+});
